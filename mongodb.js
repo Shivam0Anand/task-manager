d349220ffb35445aa711c571ee0adf3a53da8f8f
Shivam0Anand <shivam_anand@outlook.com>
2019-09-17 19:10:58 +0530

    update

--- mongodb.js
+++ mongodb.js
@@ -13,27 +13,9 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("tasks").findOne(
-      { _id: new ObjectID("5d7ca75695a2e022041e5682") },
-      (error, task) => {
-        if (error) {
-          return console.log("Unable to fetch data bhai!");
-        }
-
-        console.log(task);
-      }
-    );
-
-    db.collection("tasks")
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks);
-      });
-
-    db.collection("tasks")
-      .find({ completed: false })
-      .count((error, count) => {
-        console.log(count);
-      });
+    db.collection("users").insertOne({
+      name: "shivam",
+      age: 21
+    });
   }
 );
