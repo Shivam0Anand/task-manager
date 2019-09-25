c2a60b6b47927a7aaa65f5d23b1606fb39ddcfde
Shivam0Anand <shivam_anand@outlook.com>
2019-09-25 13:13:49 +0530

    update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,25 +8,46 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email  ");
+      }
+    }
+  },
+
   age: {
-    type: Number
+    type: Number,
+    default: 3,
+    validate(value) {
+      if (value < 2) {
+        throw new Error("Age must be greater than 2");
+      }
+    }
   }
 });
 
-// const me = new User({
-//   name: "shivam",
-//   age: 27
-// });
+const me = new User({
+  name: "  shivam  ",
+  email: "hello@hello.com"
+  // age: 27
+});
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log("Error!", error);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log("Error!", error);
+  });
 
 const Task = mongoose.model("Task", {
   discription: {
@@ -36,16 +58,16 @@ const Task = mongoose.model("Task", {
   }
 });
 
-const work = new Task({
-  discription: "Kya Pta",
-  completed: false
-});
+// const work = new Task({
+//   discription: "Kya Pta",
+//   completed: false
+// });
 
-work
-  .save()
-  .then(() => {
-    console.log(work);
-  })
-  .catch(() => {
-    console.log(error);
-  });
+// work
+//   .save()
+//   .then(() => {
+//     console.log(work);
+//   })
+//   .catch(() => {
+//     console.log(error);
+//   });
