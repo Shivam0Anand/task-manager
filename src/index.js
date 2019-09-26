fbee076822fd34ba0ef85c26f43212c9b059b4e1
Shivam0Anand <shivam_anand@outlook.com>
2019-09-26 14:00:03 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.post("/users", (req, res) => {
+  req;
   res.send("test!");
 });
 
