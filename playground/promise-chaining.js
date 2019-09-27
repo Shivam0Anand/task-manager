cfc2ceb3b26c8a0b095595c778ef7743e5bc98c1
Shivam0Anand <shivam_anand@outlook.com>
2019-09-27 15:41:25 +0530

    modified

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,2 +1,14 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
+
+User.findByIdAndUpdate("5d8b1fa31fd16f15b8c49b21", { age: 3 })
+  .then(user => {
+    console.log(user);
+    return User.countDocuments({ age: 3 });
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(e => {
+    console.log(e);
+  });
