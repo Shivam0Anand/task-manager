205068b9d4391d71fc21380bffe63671d572746a
Shivam0Anand <shivam_anand@outlook.com>
2019-09-27 19:10:28 +0530

    delete task using async/await

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5d8b132a35e9410198c8d259")
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => {
-    console.log(result);
+// Task.findByIdAndDelete("5d8b132a35e9410198c8d259")
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id, completed) => {
+  await Task.findByIdAndDelete(id);
+  const unCompletedTasks = await Task.find({ completed });
+  return unCompletedTasks;
+};
+
+deleteTaskAndCount("5d8ca62eff1ecf2f4c356a8d", false)
+  .then(unCompletedTasks => {
+    console.log(unCompletedTasks);
   })
   .catch(e => {
     console.log(e);
