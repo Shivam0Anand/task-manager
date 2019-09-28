a7f23fc78911e746860f0d73a81486d0cc60ed7e
Shivam0Anand <shivam_anand@outlook.com>
2019-09-28 19:21:56 +0530

    modified: index.js delete task

--- src/index.js
+++ src/index.js
@@ -1,183 +1,27 @@
 const express = require("express");
 require("./db/mongoose.js");
-const User = require("./models/user.js");
-const Task = require("./models/task");
+const userRouter = require("./router/user");
+const taskRouter = require("./router/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const router = new express.Router();
-router.get("/test", (req, res) => {
-  res.send("test");
-});
-app.use(router);
-// Add user
-app.post("/users", async (req, res) => {
-  //   console.log(req.body);
-  //   res.send("test ok!");
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// read users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-// Read by id
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      res.send(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(404).send(e);
-  }
-});
-
-// Update data
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every(update => {
-    allowedUpdates.includes(update);
-  });
-
-  if (!isValidOperation) {
-    return res.status(400).send("error: Invalid updates!");
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// Delete User
-
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-// Create Task
-
-app.post("/tasks", async (req, res) => {
-  //   res.send("test OK!");
-
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(200).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// Read Tasks
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).send(tasks);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-// Read Task
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.send(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-// Update Task
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["description", "completed"];
-  const isValidOperation = updates.every(update => {
-    allowedUpdates.includes(update);
-  });
-
-  if (!isValidOperation) {
-    return res.status(400).send("error: Invalid request");
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    if (!task) {
-      return res.status(400).send();
-    }
-
-    res.status(200).send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-// Delete Task
-
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      res.status(404).send();
-    }
-    res.status(200).send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("we r on port " + port);
 });
+
+const bcrypt = require("bcrypt");
+
+const pass = async () => {
+  const password = "hello!123";
+  const incryptPass = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(incryptPass);
+};
+
+pass();
