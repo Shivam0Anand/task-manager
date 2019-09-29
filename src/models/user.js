fd13ad353921d51a96f78e84baf21c44ff5ce0eb
Shivam0Anand <shivam_anand@outlook.com>
2019-09-29 13:43:15 +0530

    modified

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -10,6 +12,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -38,6 +41,47 @@ const userSchema = new mongoose.Schema({
         throw new Error("Age must be greater than 2");
       }
     }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisisshivam"); //creation of token
+
+  user.token;
+
+  return tokens;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!email) {
+    throw new Error("No account with this email!");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Wrong Passord or email");
+  }
+
+  return user;
+};
+
+// Hash Pasword
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
   }
 });
 
