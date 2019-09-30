5059b28bae2e1a9d1f75f32f348d794de44d3c79
Shivam0Anand <shivam_anand@outlook.com>
2019-09-30 12:56:02 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./router/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("Get request disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send("Under Maintainance");
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
