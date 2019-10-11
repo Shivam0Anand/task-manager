04d09ce2c7d0bb9aae568fda3fbf42c299ad8324
Shivam0Anand <shivam_anand@outlook.com>
2019-10-11 18:52:20 +0530

    limit

--- src/router/task.js
+++ src/router/task.js
@@ -34,7 +34,16 @@ router.get("/tasks", auth, async (req, res) => {
   }
 
   try {
-    await req.user.populate({ path: "tasks", match }).execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip)
+        }
+      })
+      .execPopulate();
     res.send(req.user.tasks);
     res.status(200).send(tasks);
   } catch (e) {
